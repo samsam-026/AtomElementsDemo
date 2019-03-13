29d3baa0c3ed5de065b3445faca0bde906d3e342
Sameeha Rahman <SameehaR@99x.lk>
2019-03-13 17:32:13 +0530

    Add Floating action button to demo

--- App.js
+++ App.js
@@ -8,9 +8,8 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, ScrollView, Alert } from 'react-native';
 import { Button, Input, TextArea, Picker, DatePicker, FloatingButton } from "react-native-atom-elements";
-import Icon from "react-native-vector-icons/Ionicons";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -32,30 +31,37 @@ export default class App extends Component<Props> {
   }
 
   render() {
+
+    const actions = [
+      { text: 'Accessibility', icon: "help", name: 'bt_accessibility', position: 2, onPress: () => this.handleDisplayAlert("Accessibility") },
+      { text: 'Location', icon: "pin", name: 'bt_room', position: 1, onPress: () => this.handleDisplayAlert("Location") },
+      { text: 'Video', icon: "videocam", name: 'bt_videocam', position: 3, onPress: () => this.handleDisplayAlert("Video") }
+    ];
+
     return (
       <View style={styles.container}>
-        <Button onPress={this.handleButtonClick} color="#F00" rounded outline size={"small"}>
-          <Text>Click Me</Text>
-        </Button>
-        <Input onChangeText={this.handleTextChange} placeholder="Name" />
-        <TextArea color="#0F0" onChangeText={this.handleTextChange} placeholder="Description" />
-        <Picker color="#00F" onValueChange={this.handleValueChange} selectedValue={this.state.pickerValue}>
-          <Picker.Item value="1" label="1" />
-          <Picker.Item value="2" label="2" />
-          <Picker.Item value="3" label="3" />
-        </Picker>
-        <DatePicker onDateChange={this.handleDateChange} color="#F0F" />
-        {/* <FloatingButton onPress={this.handleButtonClick} iconName={"share"}> */}
-        <FloatingButton>
-          <Button rounded onPress={this.handleButtonClick} color={"#0F0"}>
-            <Icon name={"logo-whatsapp"} size={18} />
-          </Button>
-          <Button rounded onPress={this.handleButtonClick} color={"#00F"}>
-            <Icon name={"logo-facebook"} size={18} />
-          </Button>
-        </FloatingButton>
+        <ScrollView>
+          <View style={styles.innerContainer}>
+            <Button onPress={this.handleButtonClick} color="#F00" rounded outline size={"small"}>
+              <Text>Click Me</Text>
+            </Button>
+            <Input onChangeText={this.handleTextChange} placeholder="Name" />
+            <TextArea color="#0F0" onChangeText={this.handleTextChange} placeholder="Description" />
+            <Picker color="#00F" onValueChange={this.handleValueChange} selectedValue={this.state.pickerValue}>
+              <Picker.Item value="1" label="1" />
+              <Picker.Item value="2" label="2" />
+              <Picker.Item value="3" label="3" />
+            </Picker>
+            <DatePicker onDateChange={this.handleDateChange} color="#F0F" />
+          </View>
+        </ScrollView>
+        <FloatingButton actions={actions} />
       </View>
     );
+  }
+
+  handleDisplayAlert = (btnName) => {
+    Alert.alert("Hello", btnName);
   }
 
   handleButtonClick = () => {
@@ -81,8 +87,12 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#F5FCFF',
+  },
+  innerContainer: {
+    flex: 1,
+    flexDirection: 'column',
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   }
 });
