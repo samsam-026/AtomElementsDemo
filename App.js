18db299532c2a2e75fffa439b0e72008c3fd5f6e
Sameeha Rahman <SameehaR@99x.lk>
2019-03-14 17:49:42 +0530

    Display notification in demo

--- App.js
+++ App.js
@@ -9,7 +9,7 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, ScrollView, Alert } from 'react-native';
-import { Button, Input, TextArea, Picker, DatePicker, FloatingButton } from "react-native-atom-elements";
+import { Button, Input, TextArea, Picker, DatePicker, FloatingButton, NotificationBarManager, Notification } from "react-native-atom-elements";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -30,12 +30,20 @@ export default class App extends Component<Props> {
     }
   }
 
+  componentDidMount() {
+    NotificationBarManager.registerMessageBar(this.refs.alert);
+  }
+
+  componentWillUnmount() {
+    NotificationBarManager.unregisterMessageBar();
+  }
+
   render() {
 
     const actions = [
       { text: 'Accessibility', icon: "help", name: 'bt_accessibility', position: 2, onPress: () => this.handleDisplayAlert("Accessibility") },
       { text: 'Location', icon: "pin", name: 'bt_room', position: 1, onPress: () => this.handleDisplayAlert("Location") },
-      { text: 'Video', icon: "videocam", name: 'bt_videocam', position: 3, onPress: () => this.handleDisplayAlert("Video") }
+      { text: 'Video', icon: "videocam", name: 'bt_videocam', position: 3, onPress: () => this.handleShowNotification() }
     ];
 
     return (
@@ -45,6 +53,9 @@ export default class App extends Component<Props> {
             <Button onPress={this.handleButtonClick} color="#F00" rounded outline size={"small"}>
               <Text>Click Me</Text>
             </Button>
+            {/* <Button onPress={this.handleShowNotification} color="#F00" rounded block size={"large"}>
+              <Text>Show Notification</Text>
+            </Button> */}
             <Input onChangeText={this.handleTextChange} placeholder="Name" />
             <TextArea color="#0F0" onChangeText={this.handleTextChange} placeholder="Description" />
             <Picker color="#00F" onValueChange={this.handleValueChange} selectedValue={this.state.pickerValue}>
@@ -55,9 +66,17 @@ export default class App extends Component<Props> {
             <DatePicker onDateChange={this.handleDateChange} color="#F0F" />
           </View>
         </ScrollView>
+        <Notification ref={"alert"} />
         <FloatingButton actions={actions} />
+        {/* <FloatingButton onPress={this.handleShowNotification} /> */}
       </View>
     );
+  }
+
+  handleShowNotification = () => {
+    NotificationBarManager.showAlert({
+      message: 'Your alert message goes here'
+    });
   }
 
   handleDisplayAlert = (btnName) => {
@@ -93,6 +112,6 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: "center",
     alignItems: "center",
-    paddingHorizontal: 20,
+    padding: 20,
   }
 });
