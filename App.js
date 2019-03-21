fe994c1f281fb3e0e1f92cf293bbb2dcb7f62e7a
Sameeha Rahman <SameehaR@99x.lk>
2019-03-21 15:55:02 +0530

    Create validity method for an email input and display the Forms Organism

--- App.js
+++ App.js
@@ -9,10 +9,39 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, View, ScrollView, Alert } from 'react-native';
-import { Button, FormInput, FormTextArea, FormPicker, Picker, FormDatePicker, Text, FloatingButton, NotificationBarManager, Notification, Card, ListItem, CardList } from "react-native-atom-elements";
+import {
+  Button, FormInput, FormTextArea, FormPicker, Picker, FormDatePicker, Text,
+  FloatingButton, NotificationBarManager, Notification, Card, ListItem,
+  CardList, ListView, Form
+} from "react-native-atom-elements";
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  actions = [
+    { text: 'Accessibility', image: require("./assets/images/accessibility.png"), name: 'bt_accessibility', position: 2, onPress: () => this.handleDisplayAlert("Accessibility") },
+    { text: 'Location', icon: "pin", name: 'bt_room', position: 1, onPress: () => this.handleDisplayAlert("Location") },
+    { text: 'Video', icon: "videocam", name: 'bt_videocam', position: 3, onPress: () => this.handleShowNotification() }
+  ];
+
+  listData = [
+    { title: "Heading 1", description: "Description 1", image: require("./assets/images/scenery.jpg") },
+    { title: "Heading 2", description: "Description 2", image: require("./assets/images/scenery.jpg") },
+    { title: "Heading 3", description: "Description 3", image: require("./assets/images/scenery.jpg") }
+  ];
+
+  pickerData = [
+    { value: "1", label: "1" },
+    { value: "2", label: "2" },
+    { value: "3", label: "3" }
+  ];
+
+  formElements = [
+    { label: "Full Name", type: "text", onChangeText: (value) => this.handleTextChange(value), placeholder: "John Doe" },
+    { label: "Email", type: "text", onChangeText: (value) => this.handleTextChange(value), placeholder: "john.doe@gmail.com", isValid: (value) => this.checkInputValidity(value) },
+    { label: "Type", type: "picker", onValueChange: (value) => this.handleValueChange(value), pickerData: this.pickerData },
+    { label: "Date", type: "date", onDateChange: (value) => this.handleDateChange(value) },
+    { label: "Address", type: "textarea", onChangeText: (value) => this.handleTextChange(value) },
+  ];
 
   constructor(props) {
     super(props);
@@ -32,18 +61,6 @@ export default class App extends Component<Props> {
   }
 
   render() {
-
-    const actions = [
-      { text: 'Accessibility', image: require("./assets/images/accessibility.png"), name: 'bt_accessibility', position: 2, onPress: () => this.handleDisplayAlert("Accessibility") },
-      { text: 'Location', icon: "pin", name: 'bt_room', position: 1, onPress: () => this.handleDisplayAlert("Location") },
-      { text: 'Video', icon: "videocam", name: 'bt_videocam', position: 3, onPress: () => this.handleShowNotification() }
-    ];
-
-    const ListData = [
-      { title: "Heading 1", description: "Description 1", image: require("./assets/images/scenery.jpg") },
-      { title: "Heading 2", description: "Description 2", image: require("./assets/images/scenery.jpg") },
-      { title: "Heading 3", description: "Description 3", image: require("./assets/images/scenery.jpg") }
-    ]
 
     return (
       <View style={styles.container}>
@@ -65,14 +82,16 @@ export default class App extends Component<Props> {
             </FormPicker>
             <FormDatePicker onDateChange={this.handleDateChange} color="#F0F" label="Date" />
             <Card title="Heading" description="Description" image={require("./assets/images/scenery.jpg")} />
-            <ListItem title="Heading" onPress={this.handleShowNotification} description="Description" image={require("./assets/images/scenery.jpg")} >
+            <ListItem title="Heading" description="Description" image={require("./assets/images/scenery.jpg")} >
               <ListItem title="Heading" onPress={this.handleShowNotification} description="Description" />
             </ListItem>
-            <CardList data={ListData} />
+            <CardList data={this.listData} horizontal />
+            <ListView data={this.listData} />
+            <Form formElements={this.formElements} />
           </View>
         </ScrollView>
         <Notification ref={"alert"} />
-        <FloatingButton actions={actions} />
+        <FloatingButton actions={this.actions} />
         {/* <FloatingButton onPress={this.handleShowNotification} image={require("./assets/images/accessibility.png")} /> */}
       </View>
     );
@@ -103,6 +122,11 @@ export default class App extends Component<Props> {
 
   handleDateChange = (date) => {
     console.log(date);
+  }
+
+  checkInputValidity = (text) => {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regex.test(text);
   }
 
 }
